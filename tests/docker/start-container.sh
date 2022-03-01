e4bbf3c2955dcf99d0cbbea002e2a60f206e1cb4
cw_test_server <test@newae.com>
2022-03-01 12:56:18 -0500

    Docker updates for server

--- tests/docker/start-container.sh
+++ tests/docker/start-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Container starting...Done"
+usermod -a -G plugdev cwtests
 
 su cwtests -c "cd /home/cwtests/ && python -u /home/cwtests/testing_server.py /home/cwtests/chipwhisperer /home/cwtests/tests.yaml" 2>&1
